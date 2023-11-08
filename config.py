6cdc30417edd4ceddba21a4cb7464520bf6cc895
ngoctuannguyen <ngoctuannguyen1980123@gmail.com>
2023-11-08 16:09:19 +0700

    config data change

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ CONFIG = {
     'gpu_id': "0",
     'note': 'some_note',
     'model': 'BGCN',
-    'dataset_name': 'NetEase',
+    'dataset_name': 'electronic',
     'task': 'tune',
     'eval_task': 'test',
 
